f4a460eb0daf0d6bf1b960dafddbdd59a3708ba2
Lawrence.Liu <lawrence255@yahoo.com>
2023-05-18 20:39:26 -0400

    whyyyy dont this animation work lol, not smooth??

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,6 +1,6 @@
 var animationInterval;
 var spriteSheet = document.getElementById("sprite-image");
-var widthOfSpriteSheet = 640;
+var widthOfSpriteSheet = 576;
 var widthOfEachSprite = 64;
 
 function stopAnimation() {
@@ -13,27 +13,37 @@ function startAnimation(frames, startPos, startingRow) {
   const speed = 120; //in millisecond(ms)
   const diff = widthOfEachSprite; //difference between two sprites
   var currentFrame = 0
+  var yposition = startingRow*widthOfEachSprite
 
   animationInterval = setInterval(() => {
-    spriteSheet.style.backgroundPosition = `-${position}px 0px`;
+    spriteSheet.style.backgroundPosition = `-${position}px -${yposition}px`;
+    console.log(position, yposition);
 
     if (currentFrame < frames){
-        if (position < widthOfSpriteSheet) {
+      if (position < widthOfSpriteSheet) {
         //increment the position by the width of each sprite each time
         position = position + diff;
-        } else {
-        //reset the position to show first sprite after the last one
-        position = '0px ${startingRow}*WidthOfEachSprite';
-        }
-        currentFrame ++;
+      } else {
+        //reset the position to show first sprite in row
+        position = 0;
+        yposition = yposition + widthOfEachSprite;
+        spriteSheet.style.backgroundPosition = `-${position}px -${yposition}px`;
+      }
+      currentFrame ++;
     }else{
-        currentFrame = 0;
         position = startPos;
-        spriteSheet.style.backgroundPosition = `-${position}px 0px`;
+        yposition = startingRow*widthOfEachSprite;
+        spriteSheet.style.backgroundPosition = `-${position}px -${yposition}px`;
+        currentFrame = 0;
+        //stopAnimation()
     }
-    
   }, speed);
 }
 
+function test(x, y){
+  stopAnimation()
+  spriteSheet.style.backgroundPosition = `-${x}px -${y}px`;
+}
+
 //Start animation
-startAnimation(5, 0);
+startAnimation(5, 0, 0);
